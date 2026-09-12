de9819f833161c618acd4a606e4efe635afac2cd
Mouad <mouad@gmx.net>
2026-09-12 03:31:56 +0100

    config.php changes

--- app/etc/config.php
+++ app/etc/config.php
@@ -358,6 +358,8 @@ return [
         'Magento_Wishlist' => 1,
         'Magento_WishlistAnalytics' => 1,
         'Magento_WishlistGraphQl' => 1,
+        'Hidden_Apparel' => 1,
+        'Hidden_Base' => 1,
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeCustomerBalance' => 1,
         'PayPal_BraintreeGiftCard' => 1,
