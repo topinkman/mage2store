284bd598b18c56808206bdf3f7157bb43af1e202
Mouad <mouad@gmx.net>
2026-09-12 02:09:54 +0100

    mageplaza ist entfernet von config.php

--- app/etc/config.php
+++ app/etc/config.php
@@ -358,7 +358,6 @@ return [
         'Magento_Wishlist' => 1,
         'Magento_WishlistAnalytics' => 1,
         'Magento_WishlistGraphQl' => 1,
-        'Mageplaza_Core' => 1,
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeCustomerBalance' => 1,
         'PayPal_BraintreeGiftCard' => 1,
